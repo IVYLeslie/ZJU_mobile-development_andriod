52b886fefb3923a13e95fd8ebbf87bada00bcf06
IVYLeslie <2530432911@qq.com>
2022-05-26 16:22:58 +0800

    解决json解析

--- app/src/main/java/com/example/first/AddBookActivity.java
+++ app/src/main/java/com/example/first/AddBookActivity.java
@@ -10,14 +10,19 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import com.android.volley.Request;
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class AddBookActivity extends AppCompatActivity {
     private Button button_addbook_find;
+    private Button button_addbook_add;
     private EditText input_ISBN;
     private String isbn;
     private String address = "https://api.jike.xyz/situ/book/isbn/";
@@ -26,11 +31,15 @@ public class AddBookActivity extends AppCompatActivity {
     private TextView author;
     private TextView publish;
     private TextView year;
+    private TextView error;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addbook);
         button_addbook_find=findViewById(R.id.button_addbook_find);
+        button_addbook_add=findViewById(R.id.button_addbook_add);
         input_ISBN=findViewById(R.id.input_ISBN);
         name=findViewById(R.id.text_bookname_result);
         author=findViewById(R.id.text_author_result);
@@ -44,32 +53,58 @@ public class AddBookActivity extends AppCompatActivity {
                 //isbn="9787806767245";
                 String url = address+isbn+apikey;
                 System.out.println(url);
-                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
-                        (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+                RequestQueue queues = Volley.newRequestQueue(AddBookActivity.this);
+                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET,
+                        url, null, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        System.out.println("json data===" + response);
+                        JSONObject context = null;
+                        try {
+                            context = response.getJSONObject("data");
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
 
-                            @Override
-                            public void onResponse(JSONObject response) {
-                                //textView.setText("Response: " + response.toString());
-                                //name.setText(response.data.name);
-                                System.out.println("Response: " + response.toString());
-                            }
-                        }, new Response.ErrorListener() {
+                        try {
+                            name.setText(context.getString("name"));
+                            author.setText(context.getString("author"));
+                            publish.setText(context.getString("publishing"));
+                            year.setText(context.getString("published"));
+                            button_addbook_add.setEnabled(true);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener(){
 
-                            @Override
-                            public void onErrorResponse(VolleyError error) {
-                                // TODO: Handle error
-                                System.out.println("error");
-                            }
-                        });
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                        System.out.println("发生了一个错误！");
+                        error.printStackTrace();
+
+                    }
+                });
+                queues.add(jsonObjectRequest);
 
             }
         });
 
 
-        // Access the RequestQueue through your singleton class.
-        //AddBookActivity.getInstance(this).addToRequestQueue(jsonObjectRequest);
     }
 
+//
+//    public RequestQueue getRequestQueue(){
+//        if (queues == null) {
+//            queues = Volley.newRequestQueue(getApplicationContext());
+//        }
+//        return queues;
+//    }
+//
+//    private void addToRequestQueue(JsonObjectRequest jsonObjectRequest) {
+//        getRequestQueue().add(jsonObjectRequest);
+//    }
 
 
 }
