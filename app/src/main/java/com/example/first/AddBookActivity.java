1443a62c2d610abd59e91a3e895f99c57f5c5380
IVYLeslie <2530432911@qq.com>
2022-05-29 05:17:16 +0800

    基本完成，差一个选做功能

--- app/src/main/java/com/example/first/AddBookActivity.java
+++ app/src/main/java/com/example/first/AddBookActivity.java
@@ -2,18 +2,22 @@ package com.example.first;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -26,7 +30,19 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 public class AddBookActivity extends AppCompatActivity {
@@ -35,7 +51,8 @@ public class AddBookActivity extends AppCompatActivity {
     private String address = "https://api.jike.xyz/situ/book/isbn/";
     private String apikey = "?apikey=12718.2336c4d830e490842883539cad5a9668.d596f160ca12154d2fa00b1aef5c9045";
     private ImageView image;
-
+    private Bitmap imageBitmap;
+    private JSONObject result = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,6 +60,8 @@ public class AddBookActivity extends AppCompatActivity {
         TextView error=findViewById(R.id.error);
         Button button_addbook_find=findViewById(R.id.button_addbook_find);
         Button button_addbook_add=findViewById(R.id.button_addbook_add);
+        Button add_to_list = findViewById(R.id.addbook_to_listbook);
+        Button add_to_main = findViewById(R.id.add_to_main);
         EditText input_ISBN=findViewById(R.id.input_ISBN);
         TextView name=findViewById(R.id.text_bookname_result);
         TextView author=findViewById(R.id.text_author_result);
@@ -50,7 +69,13 @@ public class AddBookActivity extends AppCompatActivity {
         TextView year=findViewById(R.id.text_publishyear_result);
         image = findViewById(R.id.image);
         image.setImageResource(R.drawable.ivy);
-
+        add_to_main.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(AddBookActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
         button_addbook_find.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -65,25 +90,25 @@ public class AddBookActivity extends AppCompatActivity {
                     @Override
                     public void onResponse(JSONObject response) {
                         System.out.println("json data===" + response);
-                        JSONObject context = null;
+
                         try {
-                            context = response.getJSONObject("data");
+                            result = response.getJSONObject("data");
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
                         try {
                             error.setText(response.getString("msg")+" !");
-                            name.setText(context.getString("name"));
-                            author.setText(context.getString("author"));
-                            publish.setText(context.getString("publishing"));
-                            year.setText(context.getString("published"));
+                            name.setText(result.getString("name"));
+                            author.setText(result.getString("author"));
+                            publish.setText(result.getString("publishing"));
+                            year.setText(result.getString("published"));
                             button_addbook_add.setEnabled(true);
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
                         try {
-                            new ImageDownloadTask().execute(context.getString("photoUrl"));
-                        } catch (Exception e) {
+                            new ImageDownloadTask().execute(result.getString("photoUrl"));
+                        } catch (JSONException e) {
                             e.printStackTrace();
                         }
                     }
@@ -101,11 +126,76 @@ public class AddBookActivity extends AppCompatActivity {
         button_addbook_add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                SharedPreferences.Editor editor = getSharedPreferences("data", MODE_PRIVATE).edit();
+                saveToLocal();
+            }
+        });
+        add_to_list.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(AddBookActivity.this,BookListActivity.class);
+                startActivity(intent);
             }
         });
 
     }
+    public String bitmapToString(Bitmap bitmap){
+        //将Bitmap转换成字符串
+        String string=null;
+        ByteArrayOutputStream bStream=new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG,100,bStream);
+        byte[]bytes=bStream.toByteArray();
+        string= Base64.encodeToString(bytes,Base64.DEFAULT);
+        return string;
+    }
+    public void saveToLocal() {
+        FileOutputStream out = null;//openFileOutput () 方法返回的是一个FileOutputStream 对象
+        BufferedWriter writer = null;
+        JSONObject jsonObject = new JSONObject();
+        String json="";
+        try {
+            jsonObject.put("author", result.getString("author"));
+            jsonObject.put("name", result.getString("name"));
+            jsonObject.put("publish", result.getString("publishing"));
+            jsonObject.put("year", result.getString("published"));
+            String imagebitmap = bitmapToString(imageBitmap);
+            jsonObject.put("image",imagebitmap);
+            json = jsonObject.toString();
+            System.out.println("json  "+json);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        try {
+            out = openFileOutput(result.getString("name"), Context.MODE_PRIVATE);
+            writer = new BufferedWriter(new OutputStreamWriter(out));
+            writer.write(json);
+        } catch (IOException | JSONException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (writer != null) {
+                    writer.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        try {
+            out = openFileOutput("catalog", Context.MODE_APPEND);
+            writer = new BufferedWriter(new OutputStreamWriter(out));
+            writer.write("#"+result.getString("name"));
+        } catch (IOException | JSONException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (writer != null) {
+                    writer.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 
     private class ImageDownloadTask extends AsyncTask<String, Void, Bitmap> {
         @Override
@@ -114,7 +204,8 @@ public class AddBookActivity extends AppCompatActivity {
             try {
                 URL imageUrl = new URL(url);
                 InputStream is = imageUrl.openConnection().getInputStream();
-                Bitmap imageBitmap = BitmapFactory.decodeStream(is);
+                imageBitmap = BitmapFactory.decodeStream(is);
+
                 return imageBitmap;
             } catch (Exception e) {
                 e.printStackTrace();
--- app/src/main/java/com/example/first/BookListActivity.java
+++ app/src/main/java/com/example/first/BookListActivity.java
@@ -1,20 +1,99 @@
 package com.example.first;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+//import com.alibaba.fastjson.JSONObject;
+
+
 
 public class BookListActivity extends AppCompatActivity {
     RecyclerView mRecyclerView;
+    private ArrayList<bookInfo> bookinfo = new ArrayList<>();
+    private Button back_main;
+    private Button list_to_add;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_booklist);
-        mRecyclerView = findViewById(R.id.recycler_view);
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-        //mRecyclerView.setAdapter(new NormalRecyclerViewAdapter(this));
+        mRecyclerView = findViewById(R.id.recyclerview);
+        back_main = findViewById(R.id.list_to_main);
+        list_to_add = findViewById(R.id.list_to_add);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(layoutManager);
+        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+        String catalog = load("catalog");
+        String[] books = catalog.split("#");
+        for (int i =1;i<books.length;i++){
+            try {
+                String info = load(books[i]);
+                JSONObject json = new JSONObject(info);
+                if (json!=null) {
+                    bookInfo tmp = new bookInfo(json.getString("name"),
+                            json.getString("author"),
+                            json.getString("publish"),
+                            json.getString("year"),
+                            json.getString("image"));
+                    bookinfo.add(tmp);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        mRecyclerView.setAdapter(new BookAdapter(bookinfo));
+        back_main.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(BookListActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        list_to_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(BookListActivity.this,AddBookActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
 
+    public String load(String path){
+        FileInputStream in = null;
+        BufferedReader reader = null;
+        StringBuilder content = new StringBuilder();
+        try {
+            in = openFileInput(path);
+            reader = new BufferedReader(new InputStreamReader(in));
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+                content.append(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return content.toString();
     }
 }
